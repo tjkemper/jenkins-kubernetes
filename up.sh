63f2566a3755e163a926abf066c9cd6c6dafcad8
Taylor Kemper <tjkemper6@gmail.com>
2016-11-19 20:30:24 -0500

    jenkins ingress

---
+++ up.sh
@@ -0,0 +1,25 @@
+
+# variables
+APP=jenkins
+NAMESPACE=operations
+
+
+# namespace
+kubectl apply -f namespaces/$NAMESPACE.yaml
+
+# nginx ingress controller
+kubectl apply -f ingress/nginx/configmap.yaml
+kubectl apply -f ingress/nginx/default-service.yaml
+kubectl apply -f ingress/nginx/default-deployment.yaml
+kubectl apply -f ingress/nginx/service.yaml
+kubectl apply -f ingress/nginx/deployment.yaml
+
+# service
+kubectl apply -f services/$APP.yaml --namespace $NAMESPACE
+
+# deployment
+kubectl apply -f deployments/$APP.yaml --namespace $NAMESPACE
+
+# ingress
+kubectl apply -f ingress/$APP.yaml
+
