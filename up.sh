9e92928f8374fee4c7a2074ea0aeae66d80c5daf
Taylor Kemper <tjkemper6@gmail.com>
2016-12-08 02:55:56 -0500

    rename config

--- up.sh
+++ up.sh
@@ -4,18 +4,8 @@ APP=jenkins
 NAMESPACE=operations
 
 
-# namespace
-kubectl apply -f namespaces/$NAMESPACE.yaml
-
-# nginx ingress controller
-kubectl apply -f ingress/nginx/configmap.yaml
-kubectl apply -f ingress/nginx/default-service.yaml
-kubectl apply -f ingress/nginx/default-deployment.yaml
-kubectl apply -f ingress/nginx/service.yaml
-kubectl apply -f ingress/nginx/deployment.yaml
-
 # secret
-kubectl create secret generic kubeconfig --from-file=./kubeconfig --namespace $NAMESPACE
+kubectl create secret generic kubeconfig --from-file=./config --namespace $NAMESPACE
 
 # service
 kubectl apply -f services/$APP.yaml --namespace $NAMESPACE
